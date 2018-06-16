ef0d44c696a3b626d064fc0e4a296487a8b6a5e8
AdithyaPrabhu <prabhu.adithya@gmail.com>
2018-06-16 22:56:58 +0530

    adding material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, ShowOnDirtyErrorStateMatcher, ErrorStateMatcher } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule, 
+    MatButtonModule, 
+    MatCheckboxModule, 
+    MatInputModule
   ],
-  providers: [],
+  providers: [
+    {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
